7f1d039337e19d23710e6c1b9122b849c7aaca8a
JollyXie <381944069@qq.com>
2018-05-01 09:34:43 +0800

    第一次全量提交

---
+++ manage.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+__author__ = 'jolly'
+__date__ = '2018/4/17 下午10:10'
+from app import app
+from flask_script import Manager
+
+#线上使用命令行启动
+manage = Manager(app)
+
+if __name__ == "__main__":
+    # app.run(debug=True, port=8000)
+    manage.run()
